71e2b5c6367fd48735b39dd3d345cf927dfbd77d
Yermain Araya <yerma.araya@gmail.com>
2021-07-11 19:43:10 -0400

    Initial commit

---
+++ src/resizer/index.ts
@@ -0,0 +1,16 @@
+import { promises as fs } from 'fs';
+import sharp from "sharp";
+
+export const resizeImage = async (filename: string, width: string, height: string): Promise<string> => {
+  const original = await sharp('./images/' + filename)
+  const [ name, extension ] = filename.split('.')
+  const newFilename = `./resized/${name}-${width}x${height}.${extension}`;
+  try {
+    await original
+      .resize(parseInt(width), parseInt(height))
+      .toFile(newFilename);
+    return newFilename;
+  } catch (e) {
+    throw new Error('Error while resizing: ' + e.message)
+  }
+}
---
+++ src/routes/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path'
+import { resizeImage } from '../resizer';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Welcome to image resizer')
+})
+
+router.get('/resize', async (req, res) => {
+  const { filename, width, height } = req.query
+  try {
+    const resizedFile = await resizeImage(filename as string, width as string, height as string)
+    res.status(200).sendFile(path.resolve(resizedFile))
+  } catch(e) {
+    res.status(500).send(e.message)
+  }
+});
+
+export default router;
