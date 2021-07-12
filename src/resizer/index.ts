bb01d5b8bd5e004b2fa0e2cae28c91a222883c77
Yerma <yerma.araya@gmail.com>
2021-07-11 21:20:03 -0400

    feat: Handle other formats (png and webp)

--- src/resizer/index.ts
+++ src/resizer/index.ts
@@ -1,14 +1,24 @@
 import { promises as fs } from 'fs';
 import sharp from 'sharp';
 
+export const getImageFileMatch = async (
+  filename: string
+): Promise<string | undefined> => {
+  const files = await fs.readdir('./images/');
+  const filenameRegex = new RegExp(`${filename}\.(jpg|png|webp)`);
+  return files.find((file) => filenameRegex.test(file));
+};
+
 export const resizeImage = async (
   filename: string,
   width: string,
   height: string
 ): Promise<string> => {
-  const original = await sharp('./images/' + filename);
-  const [name, extension] = filename.split('.');
-  const newFilename = `./resized/${name}-${width}x${height}.${extension}`;
+  const fileMatch = await getImageFileMatch(filename);
+  if (!fileMatch) throw new Error('File not found');
+
+  const original = await sharp(`./images/${fileMatch}`);
+  const newFilename = `./resized/${filename}-${width}x${height}.jpg`;
   try {
     await original
       .resize(parseInt(width), parseInt(height))
